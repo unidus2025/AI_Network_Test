7b7b8b036cbb88a758773bfccf46de667a0f70c9
unidus2025 <unidus@laposte.net>
2025-01-22 18:40:42 +0400

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,9 @@
+import json
+
+# Lire le fichier manifesto.json
+with open("manifesto.json", "r") as file:
+    data = json.load(file)
+
+print("Message:", data["message"])
+print("Challenge:", data["challenge"])
 
